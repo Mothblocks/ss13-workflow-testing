d2968fd413b703b15149e67cb86dc35b40de61b7
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2022-11-24 13:37:58 -0800

    Rerun tests if a single one fails

--- tools/pull_request_hooks/rerunFlakyTests.js
+++ tools/pull_request_hooks/rerunFlakyTests.js
@@ -1,3 +1,36 @@
+async function getFailedJobsForRun(github, context, workflowRunId, runAttempt) {
+  const { jobs } = await github.rest.actions.listJobsForWorkflowRunAttempt({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: workflowRunId,
+    attempt_number: runAttempt,
+  });
+
+  return jobs.filter((job) => job.conclusion === "failure");
+}
+
 export async function rerunFlakyTests({ github, context }) {
-  console.log("hellooo");
+  const failingJobs = await getFailedJobsForRun(
+    github,
+    context,
+    context.payload.workflow_run.id,
+    context.payload.workflow_run.run_attempt
+  );
+
+  if (failingJobs.length > 1) {
+    console.log("Multiple jobs failing. PROBABLY not flaky, not rerunning.");
+    return;
+  }
+
+  if (failingJobs.length === 0) {
+    throw new Error(
+      "rerunFlakyTests should not have run on a run with no failing jobs"
+    );
+  }
+
+  github.rest.actions.reRunWorkflowFailedJobs({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.payload.workflow_run.id,
+  });
 }
